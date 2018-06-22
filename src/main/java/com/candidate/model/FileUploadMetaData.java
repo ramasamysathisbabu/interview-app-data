413d44208c5f3f456b45b2ede9ac328c8569cf6d
ramasamysathisbabu <39998708+ramasamysathisbabu@users.noreply.github.com>
2018-06-22 16:39:33 -0400

    Upload file content to database

--- src/main/java/com/candidate/model/FileUploadMetaData.java
+++ src/main/java/com/candidate/model/FileUploadMetaData.java
@@ -1,22 +1,31 @@
 package com.candidate.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 /** An entity that stores file meta data into database*/
 @Entity
+@Table(name = "file_upload_meta_data")
 public class FileUploadMetaData {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Id
 	private long id;
 
+	@Column(name = "name")
 	private String name;
 	
+	@Column(name = "content_type")
 	private String contentType;
 	
+	@Column(name = "content_size")
 	private long contentSize;
+	
+	@Column(name = "file_content")
+	private byte[] fileContent;
 
 	public String getName() {
 		return name;
@@ -42,6 +51,12 @@ public class FileUploadMetaData {
 		this.contentSize = contentSize;
 	}
 
-	
+	public byte[] getFileContent() {
+		return fileContent;
+	}
+
+	public void setFileContent(byte[] fileContent) {
+		this.fileContent = fileContent;
+	}
 	
 }
---
+++ src/main/java/com/candidate/model/FileUploadRepository.java
@@ -0,0 +1,12 @@
+package com.candidate.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.candidate.model.FileUploadMetaData;
+
+/**
+ * A JPA repository used to perform crud operations on file meta data records in
+ * database
+ */
+public interface FileUploadRepository extends JpaRepository<FileUploadMetaData, Long> {
+}
